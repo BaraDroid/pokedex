2b2231d3dc3357bee2c9825133838ceb05b31778
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-10 14:25:30 +0100

    -

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,13 +1,30 @@
+let maxSum = 10;
+
 function init() {
-    getPkms();
+    getPkmns(maxSum);
 }
 
-async function getPkms() {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`);
+async function getPkmns(limit) {
+    let firstResponse = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit}`;
+    let response = await fetch(firstResponse);
     let currentPkmns = await response.json();
-    //hier muss es in den Array currentPkms kommen
-    for (let i = 0; i < currentPkmns.length; i++) {
+    //hier muss es in den Array currentPkms kommen?
+    console.log(currentPkmns);
+    console.log(currentPkmns.results.length);
+    
+    for (let i = 0; i < currentPkmns.results.length; i++) {
+        console.log(currentPkmns);
+        
+        //let onePkmnInfo = currentPkmns.results[i].url
         document.getElementById("mainContent").innerHTML += cardsClosedTemplate(currentPkmns, i);    
     }    
 }
 
+async function loadMorePkmns(limit) {
+    maxSum += 10;
+    getPkmns(limit);
+}
+
+//wie komme ich zu img
+//let onePkmnInfo = currentPkmns.results[i].url
+//onePkmnInfo.sprites.other.official-artwork.front_default
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,8 +1,12 @@
 
-function cardsClosedTemplate(currentPkmns, i) {
+function cardsClosedTemplate(myPkmns, j) {
+
+    console.log(`huuu huuuu ${myPkmns.results[j].name}`);
+
     return `<div class="card_closed">
-            <div class="pkm_header"><span>#${i + 1}</span><span>${currentPkmns.results[i].name}</span></div>
+            <div class="pkm_header"><span>#${j + 1}</span><span>${myPkmns.results[j].name}</span></div>
             <img src="" alt="pokemon image">
             <div class="pkm_types"></div>
-        </div>`
+        </div>`;
+        
 }
