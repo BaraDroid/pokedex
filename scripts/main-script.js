54633d942a42fed09c8efc6caafdb3e82d6df28c
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-13 11:04:18 +0100

    -

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,30 +1,57 @@
-let maxSum = 10;
+let limit = 10;
+let offset = 0;
 
 function init() {
-    getPkmns(maxSum);
+    renderFirstTenPkmns();
 }
 
-async function getPkmns(limit) {
-    let firstResponse = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit}`;
+async function renderFirstTenPkmns() {
+    offset = 0;
+    let firstResponse = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     let response = await fetch(firstResponse);
     let currentPkmns = await response.json();
-    //hier muss es in den Array currentPkms kommen?
-    console.log(currentPkmns);
-    console.log(currentPkmns.results.length);
-    
-    for (let i = 0; i < currentPkmns.results.length; i++) {
-        console.log(currentPkmns);
+    document.getElementById("mainContent").innerHTML = "";
+    for (let i = 0; i < currentPkmns.results.length; i++) { 
         
-        //let onePkmnInfo = currentPkmns.results[i].url
-        document.getElementById("mainContent").innerHTML += cardsClosedTemplate(currentPkmns, i);    
-    }    
+        document.getElementById("mainContent").innerHTML += cardsClosedTemplate(currentPkmns, i);  
+        await getPokemonId(currentPkmns, i); 
+        await getPokemonImg(currentPkmns, i);
+    }  
+    
 }
 
-async function loadMorePkmns(limit) {
-    maxSum += 10;
-    getPkmns(limit);
+async function renderNextTenPkmns() {
+    offset += 10;
+    console.log(offset);
+    let newResponse = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    let nextResponse = await fetch(newResponse);
+    let newlyLoadedPkmns = await nextResponse.json();
+    for (let k = 0; k < newlyLoadedPkmns.results.length; k++) { 
+        
+        document.getElementById("mainContent").innerHTML += cardsClosedTemplate(newlyLoadedPkmns, k);
+        await getPokemonId(newlyLoadedPkmns, k);  
+        await getPokemonImg(newlyLoadedPkmns, k);   
+    }  
 }
 
-//wie komme ich zu img
-//let onePkmnInfo = currentPkmns.results[i].url
-//onePkmnInfo.sprites.other.official-artwork.front_default
+async function getPokemonId(group, index) {
+    let onePkmnInfoSrc = `${group.results[index].url}`;
+    let response = await fetch(onePkmnInfoSrc);
+    let onePkmnInfo = await response.json();
+    console.log(onePkmnInfo.id);
+    document.getElementById(`nrOf${group.results[index].name}`).innerHTML = `# ${onePkmnInfo.id}`;
+    
+}
+
+async function getPokemonImg(group, index) {
+    let onePkmnInfoSrc = `${group.results[index].url}`;
+    let response = await fetch(onePkmnInfoSrc);
+    let onePkmnInfo = await response.json();
+    console.log(onePkmnInfo.sprites.front_default);
+    document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img src="${onePkmnInfo.sprites.front_default}" alt="picture of ${group.results[index].name}">`;
+}
+
+// function addOverlay(event) {
+//     document.getElementById("backgrounOverlay").classList.remove("d_none");
+//     event.stopPropagation();
+// }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,9 @@
 
-function cardsClosedTemplate(myPkmns, j) {
-
-    console.log(`huuu huuuu ${myPkmns.results[j].name}`);
-
-    return `<div class="card_closed">
-            <div class="pkm_header"><span>#${j + 1}</span><span>${myPkmns.results[j].name}</span></div>
-            <img src="" alt="pokemon image">
+function cardsClosedTemplate(myPkmns, z) {
+    console.log("hello" + myPkmns.results[z].name);
+    return `<div class="card_closed" onclick="addOVerlay(event)">
+            <div class="pkm_header"><span id="nrOf${myPkmns.results[z].name}"></span><span>${myPkmns.results[z].name}</span></div>
+            <div id="imgOf${myPkmns.results[z].name}"></div>
             <div class="pkm_types"></div>
-        </div>`;
-        
+        </div>`;    
 }
