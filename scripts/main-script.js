494880175847ab08acf2c511470a5f9593b36815
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-14 06:55:09 +0100

    -

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,6 +1,27 @@
 let limit = 10;
 let offset = 0;
 
+let pokemonTypes = {
+    "normal" : "../assets/imgs/normal_type.png",
+    "fighting" : "../assets/imgs/fighting_type.png",
+    "flying" : "../assets/imgs/flying_type.png",
+    "poison" : "../assets/imgs/poison_type.png",
+    "ground" : "../assets/imgs/ground_type.png",
+    "rock" : "../assets/imgs/rock_type.png",
+    "bug" : "../assets/imgs/bug_type.png",
+    "ghost" : "../assets/imgs/ghost_type.png",
+    "steel" : "../assets/imgs/steel_type.png",
+    "fire" : "../assets/imgs/fire_type.png",
+    "water" : "../assets/imgs/water_type.png",
+    "grass" : "../assets/imgs/grass_type.png",
+    "electric" : "../assets/imgs/electric_type.png",
+    "psychic" : "../assets/imgs/psychic_type.png",
+    "ice" : "../assets/imgs/ice_type.png",
+    "dragon" : "../assets/imgs/dragon_type.png",
+    "dark" : "../assets/imgs/dark_type.png",
+    "fairy" : "../assets/imgs/fairy_type.png"
+}
+
 function init() {
     renderFirstTenPkmns();
 }
@@ -12,7 +33,6 @@ async function renderFirstTenPkmns() {
     let currentPkmns = await response.json();
     document.getElementById("mainContent").innerHTML = "";
     for (let i = 0; i < currentPkmns.results.length; i++) { 
-        
         document.getElementById("mainContent").innerHTML += cardsClosedTemplate(currentPkmns, i);  
         await getPokemonId(currentPkmns, i); 
         await getPokemonImg(currentPkmns, i);
@@ -23,14 +43,14 @@ async function renderFirstTenPkmns() {
 
 async function renderNextTenPkmns() {
     offset += 10;
-    console.log(offset);
     let newResponse = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     let nextResponse = await fetch(newResponse);
     let newlyLoadedPkmns = await nextResponse.json();
     for (let k = 0; k < newlyLoadedPkmns.results.length; k++) { 
         document.getElementById("mainContent").innerHTML += cardsClosedTemplate(newlyLoadedPkmns, k);
         await getPokemonId(newlyLoadedPkmns, k);  
-        await getPokemonImg(newlyLoadedPkmns, k);   
+        await getPokemonImg(newlyLoadedPkmns, k);
+        await getPokemonType(newlyLoadedPkmns, k);   
     }  
 }
 
@@ -38,16 +58,13 @@ async function getPokemonId(group, index) {
     let onePkmnInfoSrc = `${group.results[index].url}`;
     let response = await fetch(onePkmnInfoSrc);
     let onePkmnInfo = await response.json();
-    console.log(onePkmnInfo.id);
     document.getElementById(`nrOf${group.results[index].name}`).innerHTML = `# ${onePkmnInfo.id}`;
-    
 }
 
 async function getPokemonImg(group, index) {
     let onePkmnInfoSrc = `${group.results[index].url}`;
     let response = await fetch(onePkmnInfoSrc);
     let onePkmnInfo = await response.json();
-    console.log(onePkmnInfo.sprites.front_default);
     document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img class="pkmn_img" src="${onePkmnInfo.sprites.front_default}" alt="picture of ${group.results[index].name}">`;
 }
 
@@ -55,18 +72,26 @@ async function getPokemonType(group, index) {
     let onePkmnInfoSrc = `${group.results[index].url}`;
     let response = await fetch(onePkmnInfoSrc);
     let onePkmnInfo = await response.json();
-    console.log(onePkmnInfo.types);
-    //erfahren, wie viele Typen er hat (if-else mit onePkmnInfo.types.length)
-    //pak if grass - dej tam obrazek grass
-    //if grass + water - dej dva obrazky
+    let pkmnType = onePkmnInfo.types[0].type.name;
+    let typeImg = `<img src="${pokemonTypes[pkmnType]}">`; 
+    let changingId = group.results[index].name;
+    document.getElementById(`typeOf${changingId}`).classList.add(`bg_${pkmnType}`);
     if (onePkmnInfo.types.length === 1) {
-        if (onePokemon.types[0].type.name == "normal") {
-            document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img class="pokemon_type" src="../assets/imgs/normal_type.png" alt="">`;
-        }
-        
-            return onePkmnInfo.types[0].type.name == "normal" ? document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img class="pokemon_type" src="../assets/imgs/normal_type.png" alt="">`:
-            onePkmnInfo.types[0].type.name == "fighting" ? document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img class="pokemon_type" src="../assets/imgs/fighting_type.png" alt="">`:
-            onePkmnInfo.types[0].type.name == "flying" ? document.getElementById(`imgOf${group.results[index].name}`).innerHTML = `<img class="pokemon_type" src="../assets/imgs/flying_type.png" alt="">`:
+        document.getElementById(`typeOf${changingId}`).innerHTML = typeImg;
+        document.getElementById(`imgOf${changingId}`).classList.add(`bg_${pkmnType}`);
+    }
+    else if (onePkmnInfo.types.length === 2) {
+        let pkmnType2 = onePkmnInfo.types[1].type.name;
+        let doubleImg = `<img src="${pokemonTypes[pkmnType]}"><img src="${pokemonTypes[pkmnType2]}">`;
+        document.getElementById(`typeOf${changingId}`).innerHTML = doubleImg;
+        document.getElementById(`imgOf${changingId}`).classList.add(`bg_${pkmnType}`);
+    }
+    else if (onePkmnInfo.types.length === 3) {
+        let pkmnType2 = onePkmnInfo.types[1].type.name;
+        let pkmnType3 = onePkmnInfo.types[2].type.name;
+        let tripleImg = `<img src="${pokemonTypes[pkmnType]}"><img src="${pokemonTypes[pkmnType2]}"><img src="${pokemonTypes[pkmnType3]}">`;
+        document.getElementById(`typeOf${changingId}`).innerHTML = tripleImg;
+        document.getElementById(`imgOf${changingId}`).classList.add(`bg_${pkmnType}`);
     }
 }
 
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,9 +1,8 @@
 
 function cardsClosedTemplate(myPkmns, z) {
-    console.log("hello" + myPkmns.results[z].name);
     return `<div class="card_closed" onclick="addOVerlay(event)">
             <div class="pkm_header"><span id="nrOf${myPkmns.results[z].name}"></span><span>${myPkmns.results[z].name}</span></div>
             <div id="imgOf${myPkmns.results[z].name}"></div>
-            <div class="pkm_types"></div>
+            <div id="typeOf${myPkmns.results[z].name}" class="pkm_types"></div>
         </div>`;    
 }
