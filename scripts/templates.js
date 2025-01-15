97c6b27e36098d4798dac2dc4deeca6830417653
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-15 10:22:11 +0100

    -

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,14 +1,4 @@
 
-// function cardsClosedTemplate(myPkmns, z) {
-//     return `<div class="card_closed">
-//             <div class="pkm_header"><span id="nrOf${myPkmns.results[z].name}"></span><span>${myPkmns.results[z].name}</span></div>
-//             <div id="imgOf${myPkmns.results[z].name}" onclick="addOVerlay(${event})"></div>
-//             <div id="typeOf${myPkmns.results[z].name}" class="pkm_types"></div>
-//         </div>`;    
-// }
-
-
-//pokus vzit to primo z shownPokemons - FUNGUJE TO, ALE JSEM ZASE NA ZACATKU:-D
 function cardsClosedTemplate(myPkmns, z) {
     return `<div class="card_closed">
             <div class="pkm_header"><span id="nrOf${myPkmns[z].name}"></span><span>${myPkmns[z].name}</span></div>
