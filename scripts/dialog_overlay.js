b596d7dae4897eefc914a5715389725d0721adc4
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-16 14:08:18 +0100

    -

--- scripts/dialog_overlay.js
+++ scripts/dialog_overlay.js
@@ -1,8 +1,10 @@
-
+const ctx = document.getElementById('myChart'); //wenn ich die Funktion schreibe, soll sie leer initialisiert werden und ich schreibe sie erst in meine Funktion über
+//sich globales Array zB.pokemonStats erstellen, wo wir das erstmal reinpushen und dann rausnehmen
+//also im Chart.dataser.data steht dann [pokemonStats[0], pokemonStats[1], pokemonStats[3]];
 function addOVerlay() {
     document.getElementById("backgrounOverlay").classList.remove("d_none");
     document.getElementById("theBody").classList.add("no_scroll");
-    //document.getElementById("backgrounOverlay").innerHTML = getOpenedCardTemplate();
+    document.getElementById("backgrounOverlay").innerHTML = getOpenedCardTemplate();
     event.stopPropagation();
 }
 
@@ -12,3 +14,22 @@ function closeOverlay() {
     event.stopPropagation();
 }
 
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['hp', 'attack', 'defense', 'special-attack', 'special-defense', 'speed'],
+    datasets: [{
+        label: "",
+      data: [78, 84, 78, 109, 85, 100],
+      borderWidth: 1
+    }]
+  },
+  options: {
+        indexAxis: 'y',
+        scales: {
+        y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
--- scripts/templates.js
+++ scripts/templates.js
@@ -11,6 +11,53 @@ function getEmptySearchTemplates() {
     return `<div class="empty_search">Please enter at least three letters.</div>`;
 }
 
-// function getOpenedCardTemplate() {
-//     return 
-// }
+function getOpenedCardTemplate() {
+    return `<form action="" class="overlay_content" method="dialog">
+            <div class="overlay_main_info">
+                <div class="content_left">
+                    <img  class="overlay_logo" src="./assets/logo/pokeball.png">
+                    <div class="overlay_pkm_types">
+                        <img src="./assets/imgs/fire_type.png">
+                        <img src="./assets/imgs/flying_type.png">
+                    </div>    
+                </div>  
+                <div class="content_right">
+                    <div class="overlay_header">
+                        <span class="pkmn_name">charizard</span>
+                        <span class="pkmn_id">#6</span>
+                    </div>
+                    <img class="overlay_main_image" src="./assets/charizard_probe/charizard_probe.png">
+                </div>                 
+            </div>         
+            <div class="overlay_menu">
+                <div class="menu_tab main_tab_menu">
+                    <h4>main</h4>
+                    <div class="underline u1"></div>
+                    
+                </div>
+                <div class="menu_tab stats_tab_menu">
+                    <h4>stats</h4>
+                    <div class="underline u2"></div>
+                </div>
+            </div>
+            <div class="main_tab">
+                <table>
+                    <tr><td>height:</td><td>2m</td></tr>
+                    <tr><td>weight:</td><td>100kg</td></tr>
+                    <tr><td>base exp:</td><td>263</td></tr>
+                    <tr><td>abilities:</td><td>overgrow, chlorofyl</td></tr>
+                </table>
+                <div class="arrows">
+                    <img src="./assets/vectors/arrow_left.png" alt="arrow vector">
+                    <img src="./assets/vectors/arrow_right.png" alt="arrow vector">
+                </div>
+            </div>
+            <div class="stats_tab d_none">
+                <canvas id="myChart"></canvas>
+                <div class="arrows">
+                    <img src="./assets/vectors/arrow_left.png" alt="arrow vector">
+                    <img src="./assets/vectors/arrow_right.png" alt="arrow vector">
+                </div>
+            </div>
+        </form>`
+}
